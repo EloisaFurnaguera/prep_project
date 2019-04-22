58fb00b5c0734c33e8c9f2ca7814cc713b5f1b5c
Eloisa Furnaguera <isitafurnaguera@gmail.com>
2019-04-21 17:42:28 -0700

    Add docstrings

--- prep_final_project.py
+++ prep_final_project.py
@@ -6,17 +6,17 @@ print()
 print('Pick a number from the list to choose a game or press E" to exit or "H" to comeback to main menue')
 print("1: Joke of the day")
 print("2: Feeling lucky")
-print("3: Knock knocks okes")
+print("3: Knock knocks Jokes")
 print("4: Random Joke")
 print()
 
 #------------------------------------------------------------------------------------
-#Random joke game
 
 jokes = {'A':['Today at the bank, an old lady asked me to help check her balance.', 'So I pushed her over.'], 'B':['I told my girlfriend she drew her eyebrows too high.', 'She seemed surprised.'], 'C':['I am so good at sleeping.','I can do it with my eyes closed.'], 'D':['Why is Peter Pan always flying?','He neverlands.']}
 
-
 def joke_random():
+    """ Randomly generates a joke"""
+
     print("You have chosen the Random Joke game")
     print("We give you a setup and you have to pick the right punchline")
     #Ask the user to start the game and bringing a randome setup from the dict and stops if "E" by using sys.exit() and goes back to start menue by pressing "h"
@@ -56,15 +56,12 @@ def joke_random():
             else:
                 print("That is not right")    
  
-#joke_random()
 
 #------------------------------------------------------------------------------------
-
-#Knock jokes: where they user jusk clicks and gets a random punchline
-
 Knocks = ["Adore", "Amanda", "Amish"]
 
 def knock_jokes():
+    """ User completes a random knock joke"""
     print( "You have chosen the Knocks Jokes Game")
     print("Let's have some fun whith some silly Knock, Knock jokes")
     print('if you had too much fun already press "E" to exit or "H" to retur to home')
@@ -113,16 +110,15 @@ def knock_jokes():
         else:
             print ("Try angin")                
         
-#knock_jokes()
 
 #-----------------------------------------------------------------------------------
-#Feeling lucky: the user picks a randon setup and a random punch line
 
 lucky_setups = ["Did you hear about the restaurant on the moon?","What do you call a fake noodle?", "How many apples grow on a tree?","Want to hear a joke about paper?","Why did the coffee file a police report?","Dad, did you get a haircut?"]
 
 lucky_puchlines = ["Great food, no atmosphere.","An Impasta.","All of them.","Nevermind it's tearable.", "It got mugged.", "No I got them all cut."]
 
 def feeling_lucky ():
+    """the user picks a randon setup and a random punch line"""
     print ("You have chosen the Feeling Lucky Game")
     print ()
     print ("Let's get crazy and play with random set ups and punch lines  > ")
@@ -152,11 +148,7 @@ def feeling_lucky ():
             print("I dont understand. Try aging")    
 
 
-#feeling_lucky ()
-
 #-------------------------------------------------------------------------------------
-#Joke of the day: Where the user gets a random full joke
-
 day_jokes = ["How does a penguin build its house? Igloos it together.", "Dad, did you get a haircut? No I got them all cut.", "What do you call a Mexican who has lost his car? Carlos.", "Dad, can you put my shoes on? No, I don't think they'll fit me.", "Why did the scarecrow win an award? Because he was outstanding in his field.", "Why don't skeletons ever go trick or treating? Because they have no body to go with.", "Ill call you later. Don't call me later, call me Dad."]
 
 def joke_of_the_day ():
@@ -185,8 +177,6 @@ def joke_of_the_day ():
 
 #------------------------------------------------------------------------------------
 #Start nemu
-
-
 
 def start_nemu ():
     while True:
